bc6b796051e3c37e416d722bd0029c4fb93e0304
Juansegomez <jsga008@gmail.com>
2026-02-16 19:07:34 -0500

    feat: connect DarkModeContext to Navbar and implement icon toggle
    Integrated useContext to access darkMode state and toggle function within Navbar.
    
    Implemented conditional rendering for theme icons (WbSunnyOutlinedIcon vs BedtimeOutlinedIcon).
    
    Added onClick event handler to trigger theme switching.
    
    Fixed theme persistence by syncing UI state with global context.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import './navbar.scss'
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
 import BedtimeOutlinedIcon from '@mui/icons-material/BedtimeOutlined';
 import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
@@ -7,8 +8,12 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import { Link } from 'react-router-dom';
+import { DarkModeContext } from '../../context/darkModeContext';
+import { useContext } from 'react';
 
 export const Navbar = () => {
+  const {toggle, darkMode} = useContext(DarkModeContext);
+
   return (
     <div className='navbar'>
       <div className="left">
@@ -16,7 +21,10 @@ export const Navbar = () => {
           <span>Umbral Circle</span>
         </Link>
         <HomeOutlinedIcon />
-        <BedtimeOutlinedIcon />
+        { darkMode 
+        ? <WbSunnyOutlinedIcon onClick={toggle}/>        
+        : <BedtimeOutlinedIcon onClick={toggle}/>
+        }
         <AppsOutlinedIcon />
       </div>
       <div className="search">
