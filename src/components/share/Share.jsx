4712442e1772cbb5c1ed9eddd70491dcb8bb1d3e
Juansegomez <jsga008@gmail.com>
2026-02-18 12:36:18 -0500

    feat: implement Share component and integrate it into Home page
    - Created Share component to allow users to create new posts.
    - Added share.scss with themed styles for input area and action buttons.
    - Integrated Share component between Stories and Posts in the Home view.
    - Implemented user profile picture display using currentUser from AuthContext.
    - Added stylized action items (Add Image, Map, Tag Friends) with Material UI icons.

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,47 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../context/authContext';
+import './share.scss'
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import ImageIcon from '@mui/icons-material/Image';
+import MapIcon from '@mui/icons-material/Map';
+
+export const Share = () => {
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="share">
+      <div className="container">
+        <div className="top">
+          <img
+            src={currentUser.profilePic}
+            alt=""
+          />
+          <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} />
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <input type="file" id="file" style={{display:"none"}} />
+            <label htmlFor="file">
+              <div className="item">
+                <PeopleAltIcon />
+                <span>Add Image</span>
+              </div>
+            </label>
+            <div className="item">
+              <ImageIcon />
+              <span>Add Place</span>
+            </div>
+            <div className="item">
+              <MapIcon />
+              <span>Tag Friends</span>
+            </div>
+          </div>
+          <div className="right">
+            <button>Share</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
