693c4d71ffaecc614cfb1f3633f1923ba8ab1d0d
Juansegomez <jsga008@gmail.com>
2026-02-22 18:23:21 -0500

    feat: implement modular routing and controller structure
    - Created 'routes' directory with specialized files for auth, users, posts, comments, and likes.
    - Established 'controllers' directory to decouple business logic from routing.
    - Integrated all route modules into the main index.js using Express Router.
    - Defined the initial user retrieval route (/users/find/:userId) with its respective controller placeholder.
    - Standardized the API structure following RESTful principles for better maintainability.

---
+++ back/controllers/auth.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+    res.send('Register data');
+}
+
+export const login = (req, res) => {
+    res.send('Login data');
+}
+
+export const logout = (req, res) => {
+    res.send('Logout data');
+}
--- back/index.js
+++ back/index.js
@@ -1,6 +1,18 @@
 import Express from "express";
+import authRoutes from "./routes/auth.js";
+import usersRoutes from "./routes/users.js";
+import postsRoutes from "./routes/posts.js";
+import commentsRoutes from "./routes/comments.js";
+import likesRoutes from "./routes/likes.js";
+
 
 const app = Express();
+
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+app.use("/posts", postsRoutes);
+app.use("/comments", commentsRoutes);
+app.use("/likes", likesRoutes);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
