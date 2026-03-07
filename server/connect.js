05fdb36dbb3df4b84b1cc08e30c2db27319ba485
Juansegomez <jsga008@gmail.com>
2026-03-07 18:03:00 -0500

    fix: Added SSL configuration to mysql.createPool for secure handshake.

--- server/connect.js
+++ server/connect.js
@@ -7,7 +7,9 @@ export const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME,ssl: {
+        rejectUnauthorized: false
+    },
     port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
