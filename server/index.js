4ce1a859524cdd7c0f5f761ea5567fa7e03279a0
Juansegomez <jsga008@gmail.com>
2026-07-21 16:42:44 -0500

    fix(server): resolve CORS preflight 500 error and adjust serverless configuration for Vercel
    - Updated Multer disk storage destination to write to Vercel's temporary /tmp directory in production environments.

--- server/index.js
+++ server/index.js
@@ -43,6 +43,25 @@ app.use(cors({
 }));
 app.options("*", cors());
 
+const storage = process.env.NODE_ENV === 'production'
+    ? multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, '/tmp');
+        },
+        filename: function (req, file, cb) {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+            cb(null, file.fieldname + '-' + uniqueSuffix);
+        }
+        })
+    : multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, '../client/public/upload');
+        },
+        filename: function (req, file, cb) {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+            cb(null, file.fieldname + '-' + uniqueSuffix);
+        }
+    });
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, '../client/public/upload')
